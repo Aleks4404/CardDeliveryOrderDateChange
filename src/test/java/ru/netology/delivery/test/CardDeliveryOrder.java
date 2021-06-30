dd474fe1aa07ef627343a7d28a429a043d9abc32
Aleksey Efimov <Aleks4404@mail.ru>
2021-06-30 04:56:28 +0400

    Добавлен тест "Перепланирования доствки  карты" в файл CardDeliveryOrder.java

--- src/test/java/ru/netology/delivery/test/CardDeliveryOrder.java
+++ src/test/java/ru/netology/delivery/test/CardDeliveryOrder.java
@@ -1,35 +1,39 @@
 package ru.netology.delivery.test;
 
-import com.github.javafaker.CreditCardType;
-import com.github.javafaker.Faker;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
+import com.codeborne.selenide.SelenideElement;
+
 import org.junit.jupiter.api.Test;
 import ru.netology.delivery.data.DataGenerator;
 
-import java.util.Locale;
 
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static  com.codeborne.selenide.Selenide.*;
-import static  com.codeborne.selenide.Selectors.*;
-import static  com.codeborne.selenide.Condition.*;
 
 class CardDeliveryOrder {
 
-    private Faker faker;
-
-    @BeforeEach
-    void setUpAll() {
-        faker = new Faker(new Locale("ru"));
-    }
+    String name = DataGenerator.generateName();
+    String phone = DataGenerator.generatePhone();
+    String city = DataGenerator.generateCity();
 
     @Test
-    void shouldPreventSendRequestMultipleTimes() {
-        String name = faker.name().fullName();
-        String phone = faker.phoneNumber().phoneNumber();
-        String city = faker.address().city();
-        System.out.println(name);
-        System.out.println(phone);
-        System.out.println(city);
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=city] input").setValue(city);
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(DataGenerator.generateDate(3));
+        form.$("[data-test-id=name] input").setValue(name);
+        form.$("[data-test-id=phone] input").setValue(phone);
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(".notification_status_ok").shouldBe(visible);
+        $("[data-test-id='success-notification'] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(3)));
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(DataGenerator.generateDate(7));
+        form.$(".button").click();
+        $("[data-test-id=replan-notification]").waitUntil(visible, 15000);
+        $(withText("Перепланировать")).click();
+        $(".notification_status_ok").shouldBe(visible);
+        $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(7)));
     }
 }
